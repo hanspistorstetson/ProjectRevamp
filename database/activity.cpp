1bde90058f26a3389969eab94b9c0a099fb6712e
Mason Perrella <mperrell@stetson.edu>
2017-04-16 19:07:22 -0400

    created functions and constructor for activity class

---
+++ database/activity.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include "sqlite3.h"
+#include "activity.h"
+#include <cstdlib>
+#include <string>
+#include <cstring>
+
+using namespace std;
+
+Activity::Activity() {
+    int retval;
+    
+    retval = sqlite3_open("qrlogger.db", &db);
+    if (retval != 0) {
+        cout << "Cannot open qrlogger.db: " << sqlite3_errcode(db) << endl;
+        exit(1);
+    }
+    
+    cout << "Database opened." << endl;
+
+    char* errmsg;
+    retval = sqlite3_exec(db, "CREATE TABLE IF NOT EXISTS activity (activity name, eventid, actvityid, status);", NULL, NULL, &errmsg);
+    if (retval != SQLITE_OK) {
+        cout << "Error creating event table: " << errmsg << endl;
+        sqlite3_free(errmsg);
+    }
+    
+    retval = sqlite3_exec(db, "INSERT INTO activity (name, eventid, activity id, status) values (\"Constructor Test\", \"Blah blah\", \"blah\", \"blah\");", NULL, NULL, &errmsg);
+    if (retval != SQLITE_OK) {
+        cout << "Error creating test event in constructor: " << errmsg << endl;
+        sqlite3_free(errmsg);
+    }
+}
+
+void Activity::createActivity(string activity_name, int event_id, int activity_id, string activity_status) {
+    int retval;
+
+    sqlite3_stmt *s;
+    const char *sql = "INSERT INTO activities (name, event_id, activity_id, activity_status) VALUES (?, ?, ?, ?)";
+    retval = sqlite3_prepare(db, sql, strlen(sql), &s, NULL);
+    if (retval != SQLITE_OK) {
+        cout << "Error in preparing insert statement for activity " << sqlite3_errcode(db) << endl;
+        return;
+    }
+    retval = sqlite3_bind_text(s, 1, event_name.c_str(), event_name.size(), SQLITE_STATIC);
+    if (retval != SQLITE_OK) {
+        cout << "Error in binding SQL statement " << sql << endl;
+        return;
+    }
+    retval = sqlite3_bind_text(s, 2, desc.c_str(), desc.size(), SQLITE_STATIC);
+
+
+}
+
+
+
+void Activity::setActive() {
+	status = "active";
+}
+
+void Activity::setInactive() {
+	status = "inactive";
+}
+string Activity::getStatus() {
+	return status;
+}
+int Activity:: getId() {
+	return id;
+}
+
+int Activity:: getEventId() {
+	return eventId;
+}
+void  Activity:: setId(int newid) {
+	id = newid;
+}
+void Activity:: setEventId(int newid) {
+	eventId = newid;
+}
+
+vector<Checkin> Activity:: getCheckins() {
+	return myAttendees;
+}
+
+string Activity:: getName() {
+	return name;
+}
+void Activity:: setName(string newname) {
+	name = newname;
+}
--- database/activity.h
+++ database/activity.h
@@ -1,11 +1,28 @@
 #ifndef ACTIVITY_H
 #define ACTIVITY_H
-
+#include "checkin.h"
 #include <vector>
 
-class activity {
+class Activity {
     public:
-        //TODO: Stub out methods for controller to access the database
+	Activity();
+	Activity(int eventid, int id , std::string name); //is this necessary if have createActivit?y
+  	void createActvity(std::string , int event, int id , std::string _status);//need preReqs???
+	int getId();
+	int getEventId();
+	void setId(int);
+	void setEventId(int);
+	std::vector<Checkin> getCheckins();
+//	void addToCheckins(//.....
+///	void setPreReq(Activity);
+	
+    private:
+	int id;
+	std::vector<Checkin> myAttendees;
+	int eventId;
+	std::vector<Activity> myPreReqs;//where are we declaring these?
+	std::string status;
+	std::string name;
 };
 
 #endif
