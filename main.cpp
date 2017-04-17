58c40be325d82136d2de63ec7e9574c1d8861248
Hudson DeVoe <hdevoe@stetson.edu>
2017-04-17 04:14:19 -0400

    getting rid of duplicate couts in main

--- main.cpp
+++ main.cpp
@@ -23,21 +23,19 @@ int main(int argc, char *argv[])
         cout << "Cannot open test.db: " << sqlite3_errcode(db) << endl;
         exit(1);
     }
-    cout << "Succsesfully opened." << endl;
+    cout << "Successfully opened." << endl;
 
     cout << "Creating a table..." << endl;
 
     char* errmsg;
     retval = sqlite3_exec(db, "CREATE TABLE mytable (id int, val text, x double);", NULL, NULL, &errmsg);
     if (retval != SQLITE_OK) {
-        std::cout << "Error in previous command: " << errmsg << std::endl;
         cout << "Error in previous command: " << errmsg << endl;
         sqlite3_free(errmsg);
     }
 
     retval = sqlite3_exec(db, "INSERT INTO mytable VALUES (1, \"hello\", 2.1), (2, \"goodbye\", -0.3);", NULL, NULL, &errmsg);
     if (retval != SQLITE_OK) {
-        std::cout << "Error in previous command: " << errmsg << std::endl;
         cout << "Error in previous command: " << errmsg << endl;
         sqlite3_free(errmsg);
     }
@@ -46,14 +44,12 @@ int main(int argc, char *argv[])
     const char *sql = "SELECT * FROM mytable ORDER BY id";
     retval = sqlite3_prepare(db, sql, strlen(sql), &s, NULL);
     if (retval != SQLITE_OK) {
-        std::cout << "Error in prepareing statment." << std::endl;
-        cout << "Error in prepareing statment." << endl;
+        cout << "Error in preparing statment." << endl;
     }
     while(sqlite3_step(s) == SQLITE_ROW) {
         int id = sqlite3_column_int(s, 0);
         const unsigned char *val = sqlite3_column_text(s, 1);
         double x = sqlite3_column_double(s, 2);
-        std::cout << "ID = " << id << ", val = " << val << ", x = " << x << std::endl;
         cout << "ID = " << id << ", val = " << val << ", x = " << x << endl;
     }
     sqlite3_finalize(s);
