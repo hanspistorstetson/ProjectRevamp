23f42f4b036044c1e13fd0134be45dd664a66a71
Hayden Estey <hrestey@gmail.com>
2017-04-17 16:21:09 -0400

    Small change in activity.h from clas from class

--- database/activity.h
+++ database/activity.h
@@ -5,24 +5,25 @@
 
 class Activity {
     public:
-	Activity();
-	Activity(int eventid, int id , std::string name); //is this necessary if have createActivit?y
-  	void createActvity(std::string , int event, int id , std::string _status);//need preReqs???
-	int getId();
+	//Activity(int eventid, int id , std::string name); //is this necessary if have createActivit?y
+  	static Activity* createActvity(std::string , int event, int id , std::string _status);//need preReqs???
+    static Activity* loadActivityByID(size_t activityid);
+    int getId();
 	int getEventId();
 	void setId(int);
 	void setEventId(int);
-	std::vector<Checkin> getCheckins();
+	std::vector<Checkin*> getCheckins();
 //	void addToCheckins(//.....
 ///	void setPreReq(Activity);
 	
     private:
 	int id;
-	std::vector<Checkin> myAttendees;
+	std::vector<Checkin*> myAttendees;
 	int eventId;
-	std::vector<Activity> myPreReqs;//where are we declaring these?
+	std::vector<Activity*> myPreReqs;//where are we declaring these?
 	std::string status;
 	std::string name;
+    Activity();
 };
 
 #endif
