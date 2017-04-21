59263aa160cf1844986a9f302c6827d8b87cf90b
Mason Perrella <mperrell@stetson.edu>
2017-04-21 13:59:54 -0400

    debugged activity.cpp

--- database/activity.cpp
+++ database/activity.cpp
@@ -13,30 +13,30 @@ Activity::Activity() {
 
  Activity* Activity::createActivity(string activity_name, size_t event_id, string activity_status) {
     int retval;
-
+     sqlite3* db = Database::openDatabase();
     sqlite3_stmt *s;
     const char *sql = "INSERT INTO activityTable (activity_name, event_id, activity_status) VALUES (?, ?, ?, ?)";
     retval = sqlite3_prepare(db, sql, strlen(sql), &s, NULL);
     if (retval != SQLITE_OK) {
         cout << "Error in preparing insert statement for activity " << sqlite3_errcode(db) << endl;
-        return;
+        return NULL;
     }
     retval = sqlite3_bind_text(s, 1, activity_name.c_str(), activity_name.size(), SQLITE_STATIC);
     if (retval != SQLITE_OK) {
         cout << "Error in binding SQL statement " << sql << endl;
-        return;
+        return NULL;
     }
     retval = sqlite3_bind_text(s, 2, activity_status.c_str(), activity_status.size(), SQLITE_STATIC);	
     if (retval != SQLITE_OK) {
         cout << "Error in binding SQL statement " << sql << endl;
-        return;
+        return NULL;
     }
     
    retval = sqlite3_bind_int(s, 3, event_id);	
 	
     if (retval != SQLITE_OK) {
         cout << "Error in binding SQL statement " << sql << endl;
-        return;
+        return NULL;
     }
 	//needs to return pointer to the activity created
 	Activity* a = new Activity();
@@ -63,14 +63,14 @@ size_t Activity:: getId() {
 size_t Activity:: getEventId() {
 	return eventId;
 }
-void  Activity:: setId(int newid) {
+void  Activity:: setId(size_t newid) {
 	id = newid;
 }
-void Activity:: setEventId(int newid) {
+void Activity:: setEventId(size_t newid) {
 	eventId = newid;
 }
 
-vector<Checkin> Activity:: getCheckins() {
+vector<Checkin*> Activity:: getCheckins() {
 	return myAttendees;
 }
 
--- database/activity.h
+++ database/activity.h
@@ -8,20 +8,21 @@ class Activity {
 	//Activity(int eventid, int id , std::string name); //is this necessary if have createActivit?y
   	static Activity* createActivity(std::string name, size_t eventid, std::string _status);//need preReqs???
     static Activity* loadActivityByID(size_t activityid);
-    int getId();
+    size_t getId();
 	size_t getEventId();
 	void setId(size_t);
 	void setEventId(size_t);
-//	std::vector<Checkin*> getCheckins();
-//	void addCheckins(Checkin* checkin);
+	std::vector<Checkin*> getCheckins();
+	void addCheckins(Checkin* checkin);
 	void setPreReq(Activity*);
 	void setActive();
 	void setInactive();
 	std::string getStatus();	
-    
+        std::string getName();
+        void setName(std::string);
     private:
 	size_t id;
-//	std::vector<Checkin*> myAttendees;
+	std::vector<Checkin*> myAttendees;
 	size_t eventId;
 	std::vector<Activity*> myPreReqs;//where are we declaring these?
 	std::string status;
