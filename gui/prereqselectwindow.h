433dd27358bfa8358dc23af8477580c8db7c83d3
Ryan Anderson <randerson1@stetson.edu>
2017-05-04 16:44:53 -0400

    Added functionality to prereqselectwindow w/ database

--- gui/prereqselectwindow.h
+++ gui/prereqselectwindow.h
@@ -22,4 +22,37 @@ private:
     Ui::PrereqSelectWindow *ui;
 };
 
+#endif // PREREQSELECTWINDOW_H#ifndef PREREQSELECTWINDOW_H
+#define PREREQSELECTWINDOW_H
+#include "database/activity.h"
+
+#include <QDialog>
+
+namespace Ui {
+class PrereqSelectWindow;
+}
+
+class PrereqSelectWindow : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit PrereqSelectWindow(QWidget *parent, Activity*);
+    ~PrereqSelectWindow();
+
+private slots:
+    void on_cancelButton_released();
+
+    void on_prereqSelectList_itemDoubleClicked(QListWidgetItem *item);
+
+    void on_chooseButton_released();
+
+private:
+    Ui::PrereqSelectWindow *ui;
+    std::map<int, Activity*> actMap;
+    std::vector<Activity*> totalActs;
+    std::vector<Activity*> prereqs;
+};
+
 #endif // PREREQSELECTWINDOW_H
+
