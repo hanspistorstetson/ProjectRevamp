2467283960f62f89bb1ffff2fd2e3fa55d93f819
Hans Pistor <hpistor@stetson.edu>
2017-06-12 21:03:09 -0400

    Worked a lot on homepage, styling, now working on CRUD functionality for events

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_action :store_current_location, :unless => :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
   # override the devise helper to store the current location so we can
   # redirect to it after loggin in or out. This override makes signing in
   # and signing up work automatically.
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+      user_params.permit(:firstname, :lastname, :email, :password, :password_confirmation)
+    end
+  end
+
   def store_current_location
     store_location_for(:user, request.url)
   end
